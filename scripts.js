782427e4f6ee5fe63b97de5cd954ca3c39f37af8
Skryre <zbk1998@ymail.com>
2022-10-14 23:20:32 -0400

    Just done with the game in itself, i made it so a prompt will pop up if the user click on he button. Then you can type the number of case by side you want, but if you type a number above 100 the program will register like you typed 100. For now i just need to work on the hover effect and im done with this project i think.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-let mdr =  5;
+let container = document.getElementById('container');
+
+let mdr = 60;
+
 let varOsef =  Math.pow(mdr, 2);
-console.log(varOsef)
 
 for( let i = 0; i < varOsef; i++) {
     var board = document.createElement('div');
@@ -11,7 +13,33 @@ for( let i = 0; i < varOsef; i++) {
     board.style.height = sizeDiv+"px";
     board.style.width = sizeDiv+"px";
 
-
-
-    document.getElementById('container').appendChild(board);
+    container.appendChild(board);
 }
+
+
+const button = document.querySelector('#button');
+button.addEventListener('click', () => {
+
+    container.innerHTML = '';
+    
+    let mdr = prompt("Enter a number below 100, if the number is above 100 the program will consider that you entered 100");
+    if (mdr > 100) {
+        mdr = 100
+    };
+
+    console.log(mdr);
+
+    let varOsef =  Math.pow(mdr, 2);
+    
+    for( let i = 0; i < varOsef; i++) {
+        var board = document.createElement('div');
+        board.className = "div";
+        board.setAttribute('id', 'div');
+    
+        let sizeDiv = 550/mdr;
+        board.style.height = sizeDiv+"px";
+        board.style.width = sizeDiv+"px";
+
+        container.appendChild(board);
+    }
+});
